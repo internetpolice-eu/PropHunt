2f1dc22d4beeaadef1a7510c575539d57831ab3b
Muspah <simondw@sapo.pt>
2020-10-20 23:34:12 +0100

    Hi Travis, please work

---
+++ scripts/deploy-release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+
+  echo -e "Running release script...\n"
+  echo -e "Publishing javadocs and artifacts...\n"
+  cd $HOME
+
+  rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/internetpolice-eu/PropHunt/target/mvn-repo/ \
+  travis@travis.internetpolice.eu:WWW/repo/
+
+  echo -e "Publishing javadocs...\n"
+
+  rsync -r --delete --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/internetpolice-eu/PropHunt/target/site/apidocs/ \
+  travis@travis.internetpolice.eu:WWW/javadocs/PropHunt/master/
+
+  echo -e "Publishing final plugin release...\n"
+
+  rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+  $HOME/build/internetpolice-eu/PropHunt/target/PropHunt-*.jar \
+  travis@travis.internetpolice.eu:WWW/downloads/PropHunt/
+
+fi
